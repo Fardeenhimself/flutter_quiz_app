3ab2ff9bf44d54af70352c7601558712e5927cd9
fardeenhimself <fardeenviper77@gmail.com>
2025-06-03 15:45:13 +0600

    New quizz app

---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(QuizzApp());
+
+class QuizzApp extends StatelessWidget {
+  const QuizzApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Quizzzz',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.5,
+              color: Colors.white,
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[800],
+        ),
+        body: QuizPage(),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [
+    // Icons get added automatically;
+  ];
+
+  List<String> quizQuestions = [
+    'Bangladesh is a nucleared power country',
+    'Sheikh Hasina was a prime minister',
+    'Pakistan and India are friends',
+    'Titanic sank in 1912',
+  ];
+
+  int questionNumber = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: Padding(
+              padding: EdgeInsetsGeometry.all(10.0),
+              child: Center(
+                child: Text(
+                  quizQuestions[questionNumber],
+                  style: TextStyle(fontSize: 25.0),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(15.0),
+              child: TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.green[700]),
+                onPressed: () {
+                  setState(() {
+                    // scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+
+                    questionNumber++;
+                  });
+                },
+                child: Text(
+                  'True',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(15.0),
+              child: TextButton(
+                style: TextButton.styleFrom(backgroundColor: Colors.red[700]),
+                onPressed: () {
+                  //False
+                },
+                child: Text(
+                  'False',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            child: Text(
+              'Score:',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(children: scoreKeeper),
+        ],
+      ),
+    );
+  }
+}
